97ba3b42b806e3a8000d5761e6afd9b6c9be9470
Simon Jiménez <simon@jimenez.at>
2023-04-21 15:35:29 +0200

    Update astro.config.mjs
    Prepare for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://chax-at.github.io',
+  base: '/concept-first.org',
   markdown: {
     drafts: true,
     rehypePlugins: [[addClasses, {
